c4d8ca71b0c9b0113464d2634427f12ec17f4454
SINGH ABHISHEK SHAILESH <abhishek.236427101@vcet.edu.in>
2025-10-01 02:25:56 +0530

    Refactor Header component and update user roles

--- Avkrishi/src/components/Header.jsx
+++ Avkrishi/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
           break;
         case "retailer":
           setProfileImage(retailerProfile);
-          setUsername("Retailer Ramesh");
+          setUsername("Retailer Raman");
           break;
         case "wholesaler":
           setProfileImage(wholesalerProfile);
@@ -215,3 +215,4 @@ const Header = () => {
 
 
 export default Header;
+
